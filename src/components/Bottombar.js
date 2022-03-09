0fc03558fd8530efc5f2c8e12e457f9a6ab29f02
tykavanaugh <tykavanaugh@gmail.com>
2022-03-08 23:35:46 -0500

    colors

--- src/components/Bottombar.js
+++ src/components/Bottombar.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Navbar,Nav} from 'react-bootstrap'
+import { Navbar,Nav, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { AiOutlineGithub,AiOutlineLinkedin } from 'react-icons/ai'
+import { BsStackOverflow } from 'react-icons/bs'
 
 const Bottombar = (props) => {
   return (
@@ -8,13 +10,13 @@ const Bottombar = (props) => {
   <nav className={`navbar navbar-${props.darkTheme === 'primary'? 'dark' : props.darkTheme} bg-${props.darkTheme} navbar-expand-lg`}>
     <div className="container-fluid justify-content-evenly">
       <Link to="https://github.com/tykavanaugh" style={{ textDecoration: 'none' }}>
-        <div className={`m-3 link-${props.lightTheme} `}>Github</div>
+        <Button variant={`outline-${props.lightTheme}`}>Github &nbsp;<AiOutlineGithub size={28}/></Button>
       </Link>
       <Link to="https://www.linkedin.com/in/tykavanaugh/" style={{ textDecoration: 'none' }}>
-        <div className={`m-3 link-${props.lightTheme} `}>LinkedIn</div>
+        <Button variant={`outline-${props.lightTheme}`}>LinkedIn &nbsp;<AiOutlineLinkedin size={28}/></Button>
       </Link>
       <Link to="https://stackoverflow.com/users/13619599/tyk" style={{ textDecoration: 'none' }}>
-        <div className={`m-3 link-${props.lightTheme} `}>StackOverflow</div>
+        <Button variant={`outline-${props.lightTheme}`}>Stack Overflow &nbsp;<BsStackOverflow size={28}/></Button>
       </Link>
     </div>
   </nav>
